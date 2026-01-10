c1a46061408ddaceb2b3f20fad8f1bc2f57fc6d5
leoSantos@Fedora <leosantosmelo07@gmail.com>
2026-01-10 16:45:58 -0300

    feat: primeira modelagem usuarios+tipos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { PrismaModule } from './database/prisma/prisma-module';
 import { TestModule } from './modules/tests/test.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExecutionsModule } from './modules/executions/executions.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [PrismaModule, TestModule, ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env',
-  }), ExecutionsModule,],
+  }), ExecutionsModule, UsersModule,],
   controllers: [],
   providers: [],
 })
