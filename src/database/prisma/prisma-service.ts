01ff82e274b0d860ff415b1350a507d7caf12011
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-01-10 11:37:59 -0300

    refactor: prismaService url

--- src/database/prisma/prisma-service.ts
+++ src/database/prisma/prisma-service.ts
@@ -5,15 +5,8 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
-        const adapter = new PrismaMariaDb({
-            host: process.env.DATABASE_HOST || 'localhost',
-            port: Number(process.env.DATABASE_PORT) || 3306,
-            user: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-            connectionLimit: 5,
-        })
-
+        const connectionString = process.env.DATABASE_URL;
+        const adapter = new PrismaMariaDb(connectionString)
         super({ adapter })
     }
 
